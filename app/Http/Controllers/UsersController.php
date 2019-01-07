10db3bb2c82fb552c5b3d676a7b8f3d05d7abe61
Jacob <m.a.r.t.i.n.e.z.2.2470@gmail.com>
2019-01-07 12:29:43 -0500

    first commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+
+    function index(Request $request) {
+
+        if($request->isJson()){
+            $users = User::all();
+            return response()->json($users, 200);
+        }
+        
+        return response()->json(['error' => 'Unauthorized'], 401, []);
+    }
+
+    function createUSer(Request $request) {
+        if($request->isJson()){
+            $data = $request->json()->all();
+
+            $user = User::create([
+                'name' => $data['name'],
+                'username' => $data['username'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+                'api_token' => str_random(60)
+            ]);
+
+            return response()->json($user, 201);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401, []);
+    }
+
+}
